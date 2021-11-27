5e9f96857e4a12c50fb5320d7f77b56b7705e9b1
Bui Mai Quynh Linh <bmquynhlinh@gmail.com>
2021-11-27 21:12:05 +0100

    Initial commit

---
+++ preprocess.py
@@ -0,0 +1,17 @@
+'''
+Author: BUI Mai Quynh Linh
+5K
+Description:
+-- blank database is used for external testing.
+'''
+import pandas
+
+def remove_blank():
+    
+    return None
+
+def main():
+    return None
+
+if __name__ == 'main':
+    main()

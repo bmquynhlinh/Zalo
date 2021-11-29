c3edbf53e145e78ddd0b054cbd321f86fc5169b8
Bui Mai Quynh Linh <bmquynhlinh@gmail.com>
2021-11-29 16:11:38 +0100

    extract for mask training

--- black_out.py
+++ black_out.py
@@ -5,11 +5,14 @@ Pre trained: Mobilenetv2 as backbone
 -- keep face and delete everything
 -- trained
 '''
+import os
 import cv2
 import numpy as np
 from mtcnn.mtcnn import MTCNN
 import pandas as pd
-from PIL import Image
+from matplotlib import pyplot as plt
+from face_toolbox_keras.models.detector import face_detector
+import csv
 
 
 def face_detection(img):
@@ -18,38 +21,73 @@ def face_detection(img):
     return faces
 
 
+def face_net(img):
+    fd = face_detector.FaceAlignmentDetector(
+        lmd_weights_path="./face_toolbox_keras/models/detector/s3fd/s3fd_keras_weights.h5"
+        # 2DFAN-4_keras.h5, 2DFAN-1_keras.h5
+    )
+    faces = fd.detect_face(img, with_landmarks=False)
+    return faces
+
+
+# def haar(img):
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     detector = cv2.CascadeClassifier('D:\Personal Plan\Program\Zalo\paper\haarcascade_frontalface_default.xml')
+#     faces = detector.detectMultiScale(gray, 1.3, 5)
+#     return faces
+
 def black_out(img_inv, face_box):
     mask = np.zeros_like(img_inv)
-    mask = cv2.rectangle(mask, (face_box[0], face_box[1]),(face_box[0]+face_box[2], face_box[1]+face_box[3]), (255,255,255),-1)
-    result = cv2.bitwise_xor(img_inv, mask)
+    h = np.round(face_box[2] * 0.2).astype(int)
+    print(face_box[0] - h)
+    mask = cv2.rectangle(mask, (face_box[0] - h, face_box[1] - h),
+                         (face_box[0] + face_box[2] + 2*h, face_box[1] + face_box[3] + 2*h),
+                         (255, 255, 255), -1)
+    result = cv2.bitwise_or(img_inv, mask)
     return result
 
 
 def inv_combine(img_main, img_mask):
     # img_mask[np.where((img_mask!=[0, 0, 0]).all(axis=2))] = [255,255,255]
-    cv2.imshow('name', img_mask)
-    cv2.waitKey(500)
+    # cv2.imshow('name', img_mask)
+    # cv2.waitKey(500)
     result = cv2.bitwise_and(img_main, img_mask)
     return result
 
+def save_new():
+
+    return
 
 def main():
     train_dir = './train/'
-    train_meta = train_dir + 'train_meta.csv'
+    train_meta = train_dir + 'train_meta_mask.csv'
     data = pd.read_csv(train_meta, sep=',')
-    for idx in range(2): #data.size):
-        print(data.iloc[idx, 1])
-        try:
-            img = Image.open(train_dir + 'images/' + data.iloc[idx, 1])#, cv2.COLOR_BGR2RGB) # image seems like BGR
-        except:
-            continue
-        faces = face_detection(img)
-        img_inv = np.zeros_like(img)
-        for face in faces:
-            print(face['box'])
-            img_inv = black_out(img_inv, face['box'])
-        img_save = inv_combine(img, img_inv)
-        cv2.imwrite(train_dir + 'images_inv/' + data.iloc[idx, 1], img_save)
+    if os.path.isfile('./train/train_mask_enet.csv'):
+        with open('./train/train_mask_enet.csv','w', newline='') as file:
+            write = csv.writer(file)
+            write.writerow(['frame_id','image_id', 'fname', 'mask' ])
+    with open('./train/train_mask_enet.csv', 'a', newline='') as file:
+        write = csv.writer(file)
+
+        for idx in range(5):  # data.size):4175
+            print(data.iloc[idx, 1])
+            try:
+                img = plt.imread(train_dir + 'images/' + data.iloc[idx, 1])  # , cv2.COLOR_BGR2RGB) # image seems like BGR. MTCNN trained with RGB
+            except:
+                continue
+            faces = face_detection(img)
+            img_inv = np.zeros_like(img)
+            i = 0
+            for face in faces:
+                i += 1
+                img_save = cv2.resize(face, (32, 32))
+                plt.imsave(train_dir + 'images_mask/' + data.iloc[idx, 1] + str(i), img_save)
+                write.writerow([i, data.iloc[idx, 0], data.iloc[idx, 1], data.iloc[idx, 2]])
+        #     print(face['box'])
+        #     img_inv = black_out(img_inv, face['box'])
+        # img_save = inv_combine(img, img_inv)
+        # img_save = cv2.resize(img_save, (32, 32))
+        # plt.imsave(train_dir + 'images_inv/' + data.iloc[idx, 1], img_save)
 
 
 if __name__ == '__main__':
--- train_mask.py
+++ train_mask.py
@@ -6,11 +6,35 @@ Pre trained: Mobilenetv2 as backbone
 -- trained
 '''
 
-def face_detection():
-    return None
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.efficientnet import EfficientNetB3
+import tensorflow as tf
 
-def black_out():
+
+def model_define():
+    inputs = (300, 300, 3)
+    base_model = EfficientNetB3(include_top=False, weights='imagenet', input_tensor=inputs)
+    x = tf.keras.layers.GlobalAveragePooling2D(name="avg_pool")(base_model.output)
+    x = tf.keras.layers.BatchNormalization()(x)
+    top_dropout_rate = 0.2
+    x = tf.keras.layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+    outputs = tf.keras.layers.Dense(2, activation="softmax", name="pred")(x)
+    # Compile
+    model = tf.keras.Model(inputs, outputs)
+    return model
+
+def loading(train_link):
+    
+
     return None
 
 def training():
+
     return None
+
+def main():
+    train_link = './train/train_meta_mask.csv'
+
+
+if __name__ =='__main__':
+    main()
